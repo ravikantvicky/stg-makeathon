1d48084f01382f140005f5ba9317f1cb92558ca5
Ravi Kant Pandey <ravikantvicky@gmail.com>
2018-05-30 01:25:42 +0530

    adding project

---
+++ techadv/src/main/java/com/stg/makeathon/TechadvApplication.java
@@ -0,0 +1,12 @@
+package com.stg.makeathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechadvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechadvApplication.class, args);
+	}
+}
---
+++ techadv/src/test/java/com/stg/makeathon/TechadvApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stg.makeathon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechadvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
